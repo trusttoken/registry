b5e337eefd80056216e3b2211a45109a4e2190c0
Benjamin Cosman <bcosman@caltech.edu>
2018-04-19 12:30:48 -0700

    AddressValidation.sol from TrueUSD repo now lives here

---
+++ contracts/AddressValidation.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.21;
+
+contract AddressValidation {
+    string public name = "AddressValidation";
+    mapping (address => bytes32) public keyValidations;
+    event ValidateKey(address indexed account, bytes32 indexed message);
+
+    function validateKey(bytes32 _message) public {
+        keyValidations[msg.sender] = _message;
+        emit ValidateKey(msg.sender, _message);
+    }
+}
