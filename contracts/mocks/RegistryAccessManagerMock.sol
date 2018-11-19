57f66a6ed659317c2655018d671c8854076af2da
terryli0095 <terryli0095@gmail.com>
2018-11-19 13:00:41 -0800

    store notes as fixed bytes

--- contracts/mocks/RegistryAccessManagerMock.sol
+++ contracts/mocks/RegistryAccessManagerMock.sol
@@ -1,10 +1,9 @@
 pragma solidity ^0.4.23;
 
-import "../Registry.sol";
 import "../RegistryAccessManager.sol";
 
 contract RegistryAccessManagerMock is RegistryAccessManager {
-    function confirmWrite(address /*_who*/, string /*_attribute*/, uint256 /*_value*/, string /*_notes*/, address /*_admin*/) public returns (bool) {
+    function confirmWrite(address _who, string _attribute, uint256 _value, bytes32 _notes, address _admin) public returns (bool) {
         return true;
     }
 }
