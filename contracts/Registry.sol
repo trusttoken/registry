b58f6333ad65b01f2532bb7e0215bbd96d6818ee
William Morriss <wjmelements@gmail.com>
2018-11-19 20:21:04 -0800

    restrict to view

--- contracts/Registry.sol
+++ contracts/Registry.sol
@@ -39,7 +39,7 @@ contract Registry is Claimable {
     // Allows a write if either a) the writer is that Registry's owner, or
     // b) the writer is writing to attribute foo and that writer already has
     // the canWriteTo-foo attribute set (in that same Registry)
-    function confirmWrite(address /*_who*/, bytes32 _attribute, uint256 /*_value*/, bytes32 /*_notes*/, address _admin) public returns (bool) {
+    function confirmWrite(address /*_who*/, bytes32 _attribute, uint256 /*_value*/, bytes32 /*_notes*/, address _admin) public view returns (bool) {
         return (_admin == owner || hasAttribute(_admin, keccak256(WRITE_PERMISSION ^ _attribute)));
     }
 
