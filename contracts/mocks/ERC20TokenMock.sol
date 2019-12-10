f58f9a368e10839428caeed8cab0767afc421910
William Morriss <wjmelements@gmail.com>
2019-12-10 15:56:09 -0800

    ierc20

--- contracts/mocks/ERC20TokenMock.sol
+++ contracts/mocks/ERC20TokenMock.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.5.13;
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
 contract MockToken is ERC20 {
-  constructor(address _initialAccount, uint256 _initialBalance ) public {
-    _mint(_initialAccount, _initialBalance);
-  }
+    constructor(address _initialAccount, uint256 _initialBalance ) public {
+        _mint(_initialAccount, _initialBalance);
+    }
 }
