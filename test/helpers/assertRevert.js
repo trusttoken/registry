23a8059f136333e1900d4343c84ffd2a040a2e39
Marik-D <maretskii@gmail.com>
2020-03-30 12:53:31 +0200

    Update assertRevert

--- test/helpers/assertRevert.js
+++ test/helpers/assertRevert.js
@@ -1,9 +1,13 @@
 module.exports = async function assertRevert(promise) {
+  let succeeded = false;
   try {
     await promise;
-    assert.fail('Expected revert not received');
+    succeeded = true;
   } catch (error) {
-    const revertFound = error.message.search('revert') >= 0;
+    const revertFound = error.message.search('revert') >= 0 || error.message.search('invalid opcode') >= 0 || error.message.search('invalid JUMP') >= 0;
     assert(revertFound, `Expected "revert", got ${error} instead`);
+  }
+  if(succeeded) {
+    assert.fail('Expected revert not received');
   }
 };
